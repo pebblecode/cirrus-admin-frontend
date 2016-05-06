3badfe20dbcc1ebdbb03fbf28f302e50821052a6
Dave Hillier <dave.hillier1@gmail.com>
2016-05-06 16:49:34 +0100

    added version echo

--- scripts/ebinstall.sh
+++ scripts/ebinstall.sh
@@ -1,5 +1,7 @@
 curl --silent --location https://rpm.nodesource.com/setup_4.x | sh -
 yum -y install nodejs
 yum -y install npm
-npm i
+node -v
+npm -v
+npm install
 npm run npm run frontend-build:production
